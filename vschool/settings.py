2259ea4a034aa2e847c209ace255228d49fe6000
mesho <14031981@Moh>
2022-08-01 01:32:17 +0400

    mesho2

--- vschool/settings.py
+++ vschool/settings.py
@@ -161,7 +161,7 @@ LOCALE_PATHS = [
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # BASE_DIR / 'static'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'vschool/static')
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'vschool/static'),)
 # ['vschool/static',]
 # media files
 MEDIA_URL = '/media/'
