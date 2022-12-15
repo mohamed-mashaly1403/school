abea5ef6c98bca2cba5da246a5f8b36397f260be
mesho <14031981@Moh>
2022-12-16 02:16:56 +0400

    deploy 16-12-2022 wsgi fix 4st try

--- vschool/wsgi.py
+++ vschool/wsgi.py
@@ -7,7 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
-import os
+import os , sys
+sys.path.append('/home/mesho/blog/vschool')
+sys.path.append('/home/mesho/blog/envl/Lib/site-packages')
 
 from django.core.wsgi import get_wsgi_application
 
