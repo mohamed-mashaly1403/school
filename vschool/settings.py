0ff423895c5757ea30645c9fe140ff3645eb7929
mesho <14031981@Moh>
2022-12-27 12:01:07 +0400

    deploy 27-12-2022  client_id live

--- vschool/settings.py
+++ vschool/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-02$)$2q@@@y_!u@o+4!ktm%#-kx0h4fg@p-5=0+=)1y7*w$=)t
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = []
 if DEBUG is True:
