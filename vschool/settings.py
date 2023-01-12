b5d3d5708eea0f203ced2f174e24bfc65bd3cf6b
mesho <14031981@Moh>
2023-01-12 23:54:00 +0400

    deploy 12-01-2023  try settings.ini

--- vschool/settings.py
+++ vschool/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = False
 
 # ALLOWED_HOSTS = []
 if DEBUG is True:
