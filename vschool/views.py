8173b5c9d84553a1c396faa7930dce8b449698ec
mesho <14031981@Moh>
2023-03-01 18:30:48 +0400

    deploy 01-03-2023 home improve

--- vschool/views.py
+++ vschool/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.db.models import Count
 
+from Teachers.models import TeacherProfile
 from courses.models import course, Price
 from orders.models import orderPoduct
+from users.models import account
 
 
 def handler404(request, exception):
@@ -14,6 +16,11 @@ def home(request):
     courses = orderPoduct.objects.values_list('order_course__course_name').annotate(course_count=Count('order_course')).order_by('-course_count')[:3]
     orderrr=[a_tuple[0] for a_tuple in courses]
     price = Price.objects.all()[0].minn
+    trainers = TeacherProfile.objects.filter(is_accepted=True).count()
+    course_count = course.objects.filter(is_active=True).count()
+    students_count = account.objects.filter(is_active=True,is_staff=False).count()
+
+
 
     cour = []
     for i in orderrr:
@@ -25,7 +32,11 @@ def home(request):
     context = {
         'cour': cour,
         'nbar': 'home',
-        'price':price
+        'price':price,
+        'trainers':trainers,
+        'course_count':course_count,
+        'students_count':students_count,
+
 
     }
     return render(request,'home.html',context)
